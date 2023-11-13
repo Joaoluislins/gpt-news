5abc4bf7bb70021d8d652540d0a10d84963ea224
João <joaoluislins@gmail.com>
2023-11-13 18:06:45 -0500

    hid env variables

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-openai_key = os.getenv('openai_key')
-serper_api_key = os.getenv('serper_api_key')
 import streamlit as st
 from src.ai_journalist import AIJournalist
 
@@ -12,8 +10,8 @@ def main():
         text = st.text_area('Enter here your short story:', )
         submitted = st.form_submit_button('Submit')
 
-        openai_key = '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'  # news
-        serper_api_key = 'a395f76722402c3a8b434379cd3741ba8ea13c42'
+        openai_key = os.getenv('OPENAI_KEY')
+        serper_api_key = os.getenv('SERPER_API_KEY')
 
         if submitted:
             ai_journalist = AIJournalist(openai_key, serper_api_key)
