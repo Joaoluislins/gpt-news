470c2a911b0bbfcfba978fe4684136eec9883b39
João <joaoluislins@gmail.com>
2023-11-18 17:55:02 -0500

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ from src.ai_journalist import AIJournalist
 from src.db import DB
 from datetime import datetime
 
+from google.cloud import storage
+import pandas as pd
+import os
+
 
 def main():
     st.title('🗞️ AI Journal')
@@ -32,9 +36,27 @@ def main():
                             # 'log': ''
                              }
 
-            df = DB.read()
-            df_updated = DB.write(df, data_to_append)
-            DB.update()
+            # df = DB.read()
+            # df_updated = DB.write(df, data_to_append)
+            # DB.update()
+
+            bucket_name = os.getenv('BUCKET_LOGS') 
+            blob_name = os.getenv('FILE_LOGS') 
+            client = storage.Client(project='gpt-news')
+            bucket = client.get_bucket(bucket_name)
+
+            blob = bucket.get_blob(blob_name)
+            blob.download_to_filename(blob_name)
+            df = pd.read_parquet(blob_name)
+
+            new_entry_df = pd.DataFrame([data_to_append])
+            df_new = pd.concat([df,new_entry_df])
+            df_new.reset_index(inplace=True, drop=True)     
+            df_new.to_parquet(blob_name)
+
+            blob = bucket.get_blob(blob_name)         
+            blob.upload_from_filename(blob_name)
+
 
          
 if __name__ == "__main__":
