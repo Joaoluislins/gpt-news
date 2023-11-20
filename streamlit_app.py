9fdb6bbae7260bd7f9aff63f30c75f925d72b4c4
João <joaoluislins@gmail.com>
2023-11-19 22:18:43 -0500

    timezone

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ import streamlit as st
 from src.ai_journalist import AIJournalist
 from src.db import DB
 from datetime import datetime
-from google.cloud import storage
 import pandas as pd
 import os
+from datetime import datetime
+import pytz
 
 
 def main():
@@ -20,13 +21,14 @@ def main():
         serper_api_key = os.getenv('SERPER_API_KEY')
 
         if submitted:
-            # ai_journalist = AIJournalist(openai_api_key, serper_api_key)
-            # response = ai_journalist.generate_response(text)
+            ai_journalist = AIJournalist(openai_api_key, serper_api_key)
+            response = ai_journalist.generate_response(text)
 
             response = 'response'
 
             st.info(response)
-            now = datetime.now()
+            
+            now = datetime.now(tz=pytz.timezone('US/Eastern'))
             # data to append db
             data_to_append = {
                             'datetime': f"{now}",
@@ -41,24 +43,6 @@ def main():
             df_updated = db.write(df, data_to_append)
             db.update()
           
-            # bucket_name = os.getenv('BUCKET_LOGS') 
-            # blob_name = os.getenv('FILE_LOGS') 
-            # client = storage.Client(project='gpt-news', credentials = credentials)
-            # bucket = client.get_bucket(bucket_name)
-
-            # blob = bucket.get_blob(blob_name)
-            # blob.download_to_filename(blob_name)
-            # df = pd.read_parquet(blob_name)
-
-            # new_entry_df = pd.DataFrame([data_to_append])
-            # df_new = pd.concat([df,new_entry_df])
-            # df_new.reset_index(inplace=True, drop=True)     
-            # df_new.to_parquet(blob_name)
-
-            # blob = bucket.get_blob(blob_name)         
-            # blob.upload_from_filename(blob_name)
-
-
          
 if __name__ == "__main__":
     main()
